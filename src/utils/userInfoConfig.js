a8f6db2eb9bccfa378c252c40989fcf579a19955
Abhishek <abhishekbudakoti23@gmail.com>
2025-07-23 11:46:50 +0530

    fix:resume,about

--- src/utils/userInfoConfig.js
+++ src/utils/userInfoConfig.js
@@ -3,7 +3,7 @@ import Lang from "./lang";
 const userInfo = {
   about: {
     greeting: "Hello There!",
-    aboutMe: `My name is ${Lang.name},I am from yammun nagar,Haryana and my belongings are from garhwal(uttrakhand),passed my graduation in BCA(Bachelors of computer application) in 2025 from JMIT,radaur which is affilated by kurukshetra university
+    aboutMe: `My name is ${Lang.name},I live in yammuna nagar,Haryana but i belong to garhwal(uttrakhand),Completed my graduation in BCA(Bachelors of computer application) in 2025 from JMIT,radaur which is affilated by kurukshetra university.
          I'm a passionate and self-driven web developer with a strong foundation in HTML, CSS, JavaScript, and modern frameworks like React.js. I enjoy transforming ideas into interactive and visually appealing web experiences. While I may be new to the industry, I’m constantly learning and building projects that challenge and grow my skills. I’m excited to collaborate, contribute, and grow in real-world development environments.
          `,
     skills: [
@@ -34,7 +34,7 @@ const userInfo = {
       id: 1,
       title: 'INVOICE-GENERATOR',
       description: `The Async Invoice Project is a modern, single-page application built with React.js that allows users to create, preview, and manage invoices asynchronously. It offers a smooth, responsive interface designed for freelancers, businesses, or service providers needing lightweight invoice automation.`,
-      link: 'sdlfjsdklf',
+      link: 'https://invoice.asyncit.in/',
       alter: 'async-invoice',
       image: './invoice.png',
     },
